58daa76c8de48d4f83f0828e79e4af97bc2b8ba3
Oluwatosin Olubiyi <olubiyiontheweb@gmail.com>
2021-03-23 19:37:34 +0100

    update

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+// create users
+router.get('/create', function (req, res, next) {
+  var vm = {
+    title: 'Create an account'
+  };
+  res.render('users/create', vm);
 });
 
 module.exports = router;
