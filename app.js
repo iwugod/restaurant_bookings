013bb076492bb363ef38384ea936e2b96b5badda
Joshua Iwugod <iwugodjoshua@gmail.com>
2021-03-24 20:19:06 +0400

    fix route and error handling

--- app.js
+++ app.js
@@ -22,9 +22,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+// error handler middleware
+app.use((req, res, next) => {
+  const error = new Error("Page not found");
+  error.status = 404;
+  res.render('error', {error : error});
 });
 
 // error handler
@@ -35,7 +42,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+// /* GET users listing. */
+router.get('/create', function (req, res, next) {
+  res.render('users/create', { title: 'Express users' });
 });
 
 // create users
-router.get('/create', function (req, res, next) {
+router.post('/create', function (req, res, next) {
   var vm = {
     title: 'Create an account'
   };
   res.render('users/create', vm);
+});
+
+// error handler middleware
+router.use((req, res, next) => {
+  const error = new Error("Page not found");
+  error.status = 404;
+  res.render('error', {error : error});
 });
 
 module.exports = router;
